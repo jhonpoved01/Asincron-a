4f5c0ec78a5223f7f459e2e37326f2772d444181
eliseo <eliseoamayaguerrero@gmail.com>
2026-04-27 11:25:57 -0500

    feat(apropiacion): se realiza el ejercicio 5 de apropiacion.closes #5

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ tomarDatos((datos) => {
     mostrarResultado(datosProcesados);
   });
 });
+
+//ejercicio 5 de apropiacion
+import { tomarDatos, procesarDatos, mostrarResultado } from "./procesos.js";
+
+tomarDatos()
+  .then(procesarDatos)
+  .then(mostrarResultado);
